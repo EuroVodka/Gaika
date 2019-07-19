19780666f10dd3cea6824a53b9c83d7f2fbab000
ShinobiWPS <axanderghemov@gmail.com>
2019-07-19 15:42:06 +0200

    🔨Refactor redux logic to use Redux-Starter-Kit where possible

--- src/store/actions/update.ts
+++ src/store/actions/update.ts
@@ -1,6 +1,7 @@
 import * as names from './ActionNames'
 import { KeyUpdate } from './types'
 
+/* avoid usage of redux-starter-kit here because of lack of chance of action types */
 export const updateAction = ( payload: KeyUpdate ) => ( {
 	type: names.updateAction,
 	payload,
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,6 @@
 import { configureStore, getDefaultMiddleware } from 'redux-starter-kit'
 
-import main from './reducers/main'
+import update from './reducers/update'
 
 const preloadedState = {
 	amount: 0,
@@ -8,7 +8,7 @@ const preloadedState = {
 	result: 0,
 }
 
-const reducer = main
+const reducer = update
 
 const middleware = [...getDefaultMiddleware()]
 
@@ -16,38 +16,5 @@ export const store = configureStore( {
 	reducer,
 	middleware,
 	devTools: process.env.NODE_ENV !== 'production',
-	// ts error on preloadedState
-	preloadedState: preloadedState as object,
+	preloadedState,
 } )
-
-/* SAMPLE NORMAL REDUX STORE CONF
-import { applyMiddleware, compose, createStore } from 'redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import thunkMiddleware from 'redux-thunk'
-
-import monitorReducersEnhancer from './enhancers/monitorReducers'
-import rootReducer from './reducers'
-
-const preloadedState = {
-	amount: 0,
-	ratio: 0,
-	result: 0,
-}
-
-
-export default function configureStore(preloadedState) {
-  const middlewares = [ thunkMiddleware]
-  const middlewareEnhancer = applyMiddleware(...middlewares)
-
-  const enhancers = [middlewareEnhancer, monitorReducersEnhancer]
-  const composedEnhancers = composeWithDevTools(...enhancers)
-
-  const store = createStore(rootReducer, preloadedState, composedEnhancers)
-
-  if (process.env.NODE_ENV !== 'production' && module.hot) {
-    module.hot.accept('./reducers', () => store.replaceReducer(rootReducer))
-  }
-
-  return store
-}
-*/
